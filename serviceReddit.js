b79c2cc78c3fe752e68d07e90521d8946194ed32
sethdame <seth.e.dame@gmail.com>
2015-10-22 02:58:11 -0600

    finished

---
+++ serviceReddit.js
@@ -0,0 +1,38 @@
+var app = angular.module('reddit');
+
+app.service('FirebaseService', function($http, $q){
+	
+
+	this.getPosts = function(){
+		var myPromise = $q(function(resolve, reject){
+			$http.get("https://devmtn.firebaseio.com/posts.json").then(function(response){
+     			resolve(response.data);
+			}, reject);
+		});
+		return myPromise;
+		// The simple elegant way:
+     	// $http.get("https://devmtn.firebaseio.com/posts.json").then(function(response){
+     	// 	return response.data
+     	// 	}); 
+  	};
+
+  	this.addPost = function(post){
+  		post.timestamp = date.now();
+  		post.comments = [];
+  		post.karma = 0;
+  		post.id = guid();
+  	}
+
+  	var guid = function() {
+    var s4 = function() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
+	
+
+
+})
